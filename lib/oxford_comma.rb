de786e6c50cdc587216143c0fb8fdaaa4aeacbe1
Vikram Chatterjee <strycora@gmail.com>
2020-11-10 17:20:28 -0700

    commit tree 165cc90c485fe6601473328b11956121f8097d8c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(items) 
+  if items.length == 1
+    items.join 
+  elsif items.length == 2
+    items.join(" and ")
+  elsif items.length > 2 
+    items[-1] = "and " + items[-1]
+    items.join(", ")
+  else
+    " "
+  end
 end
